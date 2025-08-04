cee28d00674345b1ac233f4b9144d33b708bbd3c
Kevin Rozario <rozariokevin73@gmail.com>
2025-08-04 14:29:32 +0530

    feat: update backend structure with server and app initialization

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,10 @@
-// start
+import app from "./app.js";
+import { config } from "dotenv";
+
+config({ path: ".env" });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running...\nLocal: http://localhost:${PORT}`);
+});
