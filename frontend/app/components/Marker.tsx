baca3bb5b42755deef666061ee37ac2551a110e0
Kevin Rozario <rozariokevin73@gmail.com>
2025-08-06 01:27:37 +0530

    feat: remove logo files and welcome component; add Google Maps integration with pharmacy markers
    - Deleted logo-light.svg and welcome.tsx components.
    - Added @vis.gl/react-google-maps dependency for Google Maps integration.
    - Created Marker and MarkerInfoCard components to display pharmacy locations on the map.
    - Implemented Map component to render Google Map with pharmacy markers.
    - Added pharmacies.json data file containing pharmacy details.
    - Defined IPharmacy interface for type safety in pharmacy data.

---
+++ frontend/app/components/Marker.tsx
@@ -0,0 +1,42 @@
+import { AdvancedMarker, InfoWindow } from "@vis.gl/react-google-maps";
+import { useCallback, useState } from "react";
+import MarkerInfoCard from "./MarkerInfoCard";
+
+const Marker = ({ pharmacies }: { pharmacies: IPharmacy[] }) => {
+  const [selectedPharmacy, setSelectedPharmacy] = useState<IPharmacy | null>(
+    null
+  );
+
+  const handleMarkerClick = useCallback((pharmacy: IPharmacy) => {
+    setSelectedPharmacy(pharmacy);
+  }, []);
+
+  const handleCloseInfoWindow = useCallback(() => {
+    setSelectedPharmacy(null);
+  }, []);
+
+  return (
+    <>
+      {pharmacies.map((pharmacy) => (
+        <AdvancedMarker
+          key={pharmacy.pharmacyId}
+          position={pharmacy.location}
+          clickable={true}
+          onClick={() => handleMarkerClick(pharmacy)}
+        />
+      ))}
+
+      {selectedPharmacy && (
+        <InfoWindow
+          position={selectedPharmacy.location}
+          onCloseClick={handleCloseInfoWindow}
+          pixelOffset={[0, -30]}
+        >
+          <MarkerInfoCard selectedPharmacy={selectedPharmacy} />
+        </InfoWindow>
+      )}
+    </>
+  );
+};
+
+export default Marker;
---
+++ frontend/types/types.d.ts
@@ -0,0 +1,8 @@
+interface IPharmacy {
+  pharmacyId: number;
+  pharmacyName: string;
+  rating: number;
+  phoneNumber: string;
+  location: google.maps.LatLngLiteral;
+  address: string;
+}
