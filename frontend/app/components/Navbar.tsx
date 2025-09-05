d63943b7fe36e546cb7347b69fcc77e63f70a78c
Kevin Rozario <rozariokevin73@gmail.com>
2025-09-05 13:10:36 +0530

    feat: implement Navbar component with active state management for navigation links

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
+  const [active, setActive] = useState("home");
+
   return (
-    <div className="p-2 bg-[#EAEDEC] flex justify-between items-center w-[36vw] rounded-full text-black absolute top-4 left-[32%]">
-      <div className="bg-white rounded-full px-6 py-2">Home</div>
-      <div className="rounded-full px-6 py-2">Map</div>
-      <div className="rounded-full px-6 py-2">About</div>
-    </div>
+    <>
+      <div className="px-2 py-2 bg-[#EAEDEC] flex justify-between items-center w-[36vw] rounded-full text-black absolute top-4 left-[32%]">
+        <NavLink
+          to="/"
+          className={`rounded-full px-4 py-2 ${active === "home" ? "bg-green-600 text-white" : "text-black"}`}
+          onClick={() => setActive("home")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/map"
+          className={`rounded-full px-4 py-2 ${active === "map" ? "bg-green-600 text-white" : "text-black"}`}
+          onClick={() => setActive("map")}
+        >
+          Map
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={`rounded-full px-4 py-2 ${active === "about" ? "bg-green-600 text-white" : "text-black"}`}
+          onClick={() => setActive("about")}
+        >
+          About
+        </NavLink>
+      </div>
+
+      {/* Mobile View */}
+    </>
   );
 };
 
